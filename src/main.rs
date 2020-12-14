719ab0d20df5cdb740cda9f3f7d62c59fe825a5d
takubokudori <takubokudori@gmail.com>
2020-12-15 00:55:48 +0900

    typo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 // Copyright takubokudori.
-// This source code is licensed under the MIT or Apache-2 license.
+// This source code is licensed under the MIT or Apache-2.0 license.
 use clap::{App, Arg, ArgMatches};
-use std::io::Write;
 use hvctrl::hyperv::HyperVCmd;
+use std::io::Write;
 
 fn input(m: &ArgMatches, name: &str) -> String {
     match m.value_of(name) {
